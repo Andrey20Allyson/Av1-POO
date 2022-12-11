0cc69f8cb1f649c65efcc12471dda35faeadb37d
Andrey Allyson <andreyuser47@gmail.com>
2022-12-11 01:35:57 -0300

    main method refactor

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,11 @@ public class Main {
         // Isso é apenas um código base.
         EnvironmentStorage storage = new EnvironmentStorage();
 
-        for (int i = 0; i < 12; i++) {
+        // Numero de registros a serem criados.
+        final int NUM_OF_REGISTERS = 48;
+
+        // Cria N registros aleatórios.
+        for (int i = 0; i < NUM_OF_REGISTERS; i++) {
             var temp = Math.random() * 100;
             var carb = Math.random() * 100;
             var humi = Math.random() * 100;
@@ -17,8 +21,25 @@ public class Main {
             storage.createRegistry((float) temp, (float) carb, (float) humi, Calendar.getInstance().getTime());
         }
 
-        String formattedTable = storage.toString();
+        // Ordena os registros pela temperatura.
+        storage.sortByTemperature();
 
+        // Lista de campos permitidos.
+        final String[] ALLOWED = {
+            "temperature",
+            // "carbonDioxideQnt",
+            // "humidity",
+            "time",
+        };
+
+        // Variaveis de inicio e fim da tabela.
+        final int START = 0;
+        final int END = -1;
+
+        // Gera a String em formato de tabela.
+        String formattedTable = storage.toString(START, END, ALLOWED);
+
+        // Exibe os dados na tela.
         System.out.println(formattedTable);
     }
 }
