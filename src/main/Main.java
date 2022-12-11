976b80dfe5d59141d154c8c6029d289e4cd95155
Andrey Allyson <andreyuser47@gmail.com>
2022-12-10 21:07:37 -0300

    ilustration

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,23 @@
 package main;
 
+import java.util.Calendar;
+
+import dataStorer.EnvironmentStorage;
+
 public class Main {
     public static void main(String[] args) {
-        
+        EnvironmentStorage storage = new EnvironmentStorage();
+
+        for (int i = 0; i < 12; i++) {
+            var temp = Math.random() * 100;
+            var carb = Math.random() * 100;
+            var humi = Math.random() * 100;
+
+            storage.createRegistry((float) temp, (float) carb, (float) humi, Calendar.getInstance().getTime());
+        }
+
+        String formattedTable = storage.toString();
+
+        System.out.println(formattedTable);
     }
 }
